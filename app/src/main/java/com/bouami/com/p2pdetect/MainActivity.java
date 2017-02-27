73aeee4d6eb917a5d642f33ed2e42e205d18ca9e
mbouami <mbouami@gmail.com>
2017-02-28 00:50:04 +0100

    Update MainActivity.java

--- app/src/main/java/com/bouami/com/p2pdetect/MainActivity.java
+++ app/src/main/java/com/bouami/com/p2pdetect/MainActivity.java
@@ -10,6 +10,7 @@ import android.net.wifi.p2p.WifiP2pConfig;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.ConnectionInfoListener;
 import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceInfo;
 import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceRequest;
 import android.os.Build;
@@ -25,30 +26,31 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bouami.com.p2pdetect.WiFiDirectServicesList.WiFiDevicesAdapter;
-import com.google.android.gms.common.api.GoogleApiClient;
-
+import com.bouami.com.p2pdetect.WiFiDirectServicesList.DeviceClickListener;
+import com.bouami.com.p2pdetect.WiFiChatFragment.MessageTarget;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DeviceClickListener, Handler.Callback, MessageTarget,
+        ConnectionInfoListener {
 
     private final int REQUEST_PERMISSION_STATE = 1;
     public static final String TAG = "p2pdetect";
     // TXT RECORD properties
-    public static final String TXTRECORD_PROP_AVAILABLE = "available";
+    public static final String TXTRECORD_PROP_AVAILABLE = "ETAT";
     public static final String SERVICE_INSTANCE = "_wifidemotest";
     public static final String SERVICE_REG_TYPE = "_presence._tcp";
     public static final int MESSAGE_READ = 0x400 + 1;
     public static final int MY_HANDLE = 0x400 + 2;
     private WifiP2pManager mManager;
     static final int SERVER_PORT = 4545;
-    private IntentFilter mIntentFilter;
+    private IntentFilter mIntentFilter = new IntentFilter();
     private WifiP2pManager.Channel mChannel;
-    private WiFiDirectBroadcastReceiver mReceiver = null;
     private WifiP2pDnsSdServiceRequest serviceRequest;
     //    private Handler handler = new Handler(this);
+    private WiFiDirectBroadcastReceiver mReceiver = null;
     private WiFiChatFragment chatFragment;
     private WiFiDirectServicesList servicesList = null;
     private TextView statusTxtView;
@@ -70,21 +72,14 @@ public class MainActivity extends AppCompatActivity {
         }
         setContentView(R.layout.main);
         statusTxtView = (TextView) findViewById(R.id.status_text);
-        mIntentFilter = new IntentFilter();
         mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
         mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
         mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
         mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
         mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
         mChannel = mManager.initialize(this, getMainLooper(), null);
-//        mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
-//        ChetcherVoisin();
-//        startRegistration();
         startRegistrationAndDiscovery();
-        servicesList = new WiFiDirectServicesList();
-//        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-//        transaction.add(R.id.container_root,new WiFiDirectServicesList(),"services").commit();
-        getSupportFragmentManager().beginTransaction().add(R.id.container_root, servicesList, "services").commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.container_root, new WiFiDirectServicesList(), "services").commit();
         discoverService();
     }
 
@@ -102,37 +97,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-//    @Override
-//    public void onResume() {
-//        super.onResume();
-//        mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
-//        registerReceiver(mReceiver, mIntentFilter);
-//    }
-//
-//    @Override
-//    public void onPause() {
-//        super.onPause();
-//        unregisterReceiver(mReceiver);
-//    }
-
-    public void ChetcherVoisin() {
-        mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
-            @Override
-            public void onSuccess() {
-//                Log.d(TAG, "discoverPeers success");
-                appendStatus("discoverPeers success");
-                new mTaost(getBaseContext(), "discoverPeers success");
-            }
-
-            @Override
-            public void onFailure(int reason) {
-//                Log.d(TAG, "discoverPeers onFailure");
-                new mTaost(getBaseContext(), "discoverPeers onFailure");
-                appendStatus("discoverPeers onFailure");
-            }
-        });
-    }
-
     public Handler getHandler() {
         return handler;
     }
@@ -140,37 +104,6 @@ public class MainActivity extends AppCompatActivity {
     public void setHandler(Handler handler) {
         this.handler = handler;
     }
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//            ActivityCompat.requestPermissions(this,
-//                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
-//                            Manifest.permission.ACCESS_WIFI_STATE,
-//                            Manifest.permission.CHANGE_WIFI_STATE,
-//                            Manifest.permission.INTERNET,
-//                            Manifest.permission.ACCESS_NETWORK_STATE,
-//                            Manifest.permission.ACCESS_FINE_LOCATION},
-//                    REQUEST_PERMISSION_STATE);
-//        }
-//        setContentView(R.layout.main);
-//        statusTxtView = (TextView) findViewById(R.id.status_text);
-//        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-//        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-//        mIntentFilter
-//                .addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-//        mIntentFilter
-//                .addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-//        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-//        mChannel = mManager.initialize(this, getMainLooper(), null);
-//        startRegistrationAndDiscovery();
-//        servicesList = new WiFiDirectServicesList();
-//        getFragmentManager().beginTransaction()
-//                .add(R.id.container_root, servicesList, "services").commit();
-//    }
-//
-
 
     @Override
     protected void onRestart() {
@@ -200,15 +133,14 @@ public class MainActivity extends AppCompatActivity {
      */
     private void startRegistrationAndDiscovery() {
         Map<String, String> record = new HashMap<String, String>();
-        record.put("listenport", String.valueOf(SERVER_PORT));
-        record.put("buddyname", "Mohammed BOUAMI" + (int) (Math.random() * 1000));
-//        record.put("available", "visible");
+        record.put("PORT_UTILISE", String.valueOf(SERVER_PORT));
+        record.put("ADMINISTRATEUR", "Mohammed BOUAMI" + (int) (Math.random() * 1000));
         record.put(TXTRECORD_PROP_AVAILABLE, "visible");
         WifiP2pDnsSdServiceInfo service = WifiP2pDnsSdServiceInfo.newInstance(SERVICE_INSTANCE, SERVICE_REG_TYPE, record);
         mManager.addLocalService(mChannel, service, new WifiP2pManager.ActionListener() {
             @Override
             public void onSuccess() {
-                appendStatus("Added Local Service");
+                appendStatus("Enregistrement du service local");
             }
 
             @Override
@@ -237,9 +169,7 @@ public class MainActivity extends AppCompatActivity {
         WifiP2pManager.DnsSdTxtRecordListener txtListener = new WifiP2pManager.DnsSdTxtRecordListener() {
             @Override
             public void onDnsSdTxtRecordAvailable(String fullDomain, Map record, WifiP2pDevice device) {
-                Log.d(TAG, "DnsSdTxtRecord available -" + record.toString());
-//                buddies.put(device.deviceAddress, (String) record.get("buddyname"));
-                appendStatus("DnsSdTxtRecord available -" + record.toString());
+                appendStatus("DnsSdTxtRecord disponible - " + record.toString());
             }
         };
         WifiP2pManager.DnsSdServiceResponseListener servListener = new WifiP2pManager.DnsSdServiceResponseListener() {
@@ -248,7 +178,7 @@ public class MainActivity extends AppCompatActivity {
 
                 // Update the device name with the human-friendly version from
                 // the DnsTxtRecord, assuming one arrived.
-                        appendStatus("instanceName :"+instanceName);
+                appendStatus("Nom de session :"+instanceName);
                 // Update the device name with the human-friendly version from
                 // the DnsTxtRecord, assuming one arrived.
 
@@ -300,9 +230,10 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    //    @Override
+    @Override
     public void connectP2p(WiFiP2pService service) {
         WifiP2pConfig config = new WifiP2pConfig();
+        final WiFiP2pService mService = service;
         config.deviceAddress = service.device.deviceAddress;
         config.wps.setup = WpsInfo.PBC;
         if (serviceRequest != null)
@@ -318,16 +249,16 @@ public class MainActivity extends AppCompatActivity {
         mManager.connect(mChannel, config, new WifiP2pManager.ActionListener() {
             @Override
             public void onSuccess() {
-                appendStatus("Connecting to service");
+                appendStatus("Connecté au service : "+mService.device.deviceName);
             }
             @Override
             public void onFailure(int errorCode) {
-                appendStatus("Failed connecting to service");
+                appendStatus("Erreur de Connection au service");
             }
         });
     }
 
-//    @Override
+    @Override
     public boolean handleMessage(Message msg) {
         switch (msg.what) {
             case MESSAGE_READ:
@@ -343,21 +274,23 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
-//    @Override
-//    public void onResume() {
-//        super.onResume();
-////        mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
-//        registerReceiver(mReceiver, mIntentFilter);
-//    }
-//    @Override
-//    public void onPause() {
-//        super.onPause();
-//        unregisterReceiver(mReceiver);
-//    }
 
-//    @Override
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
+        registerReceiver(mReceiver, mIntentFilter);
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
+    @Override
     public void onConnectionInfoAvailable(WifiP2pInfo p2pInfo) {
         Thread handler = null;
+        Log.d(TAG, "onConnectionInfoAvailable");
         /*
          * The group owner accepts connections using a server socket and then spawns a
          * client socket for every client. This is handled by {@code
@@ -376,56 +309,19 @@ public class MainActivity extends AppCompatActivity {
             }
         } else {
             Log.d(TAG, "Connected as peer");
-            handler = new ClientSocketHandler(
-                    ((WiFiChatFragment.MessageTarget) this).getHandler(),
-                    p2pInfo.groupOwnerAddress);
+            handler = new ClientSocketHandler(((WiFiChatFragment.MessageTarget) this).getHandler(),p2pInfo.groupOwnerAddress);
             handler.start();
         }
         chatFragment = new WiFiChatFragment();
         getSupportFragmentManager().beginTransaction().replace(R.id.container_root, chatFragment).commit();
         statusTxtView.setVisibility(View.GONE);
+
     }
 
     public void appendStatus(String status) {
         String current = statusTxtView.getText().toString();
         statusTxtView.setText(current + "\n" + status);
     }
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-//    public Action getIndexApiAction() {
-//        Thing object = new Thing.Builder()
-//                .setName("Main Page") // TODO: Define a title for the content shown.
-//                // TODO: Make sure this auto-generated URL is correct.
-//                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-//                .build();
-//        return new Action.Builder(Action.TYPE_VIEW)
-//                .setObject(object)
-//                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-//                .build();
-//    }
-//
-//    @Override
-//    public void onStart() {
-//        super.onStart();
-//
-//        // ATTENTION: This was auto-generated to implement the App Indexing API.
-//        // See https://g.co/AppIndexing/AndroidStudio for more information.
-//        client.connect();
-//        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-//    }
-//
-//    @Override
-//    public void onStop() {
-//        super.onStop();
-//
-//        // ATTENTION: This was auto-generated to implement the App Indexing API.
-//        // See https://g.co/AppIndexing/AndroidStudio for more information.
-//        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-//        client.disconnect();
-//    }
 
     public class mTaost {
         public mTaost(Context baseContext, String s) {
